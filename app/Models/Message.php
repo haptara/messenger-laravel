a5314525a733b1a657af60629bebf6fd805afdb6
Haptara <fikriisfluffy@gmail.com>
2024-12-11 10:38:33 +0700

    first app

---
+++ app/Models/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "sender_id",
+        "receiver_id",
+        "content"
+    ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id', 'id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id', 'id');
+    }
+}
